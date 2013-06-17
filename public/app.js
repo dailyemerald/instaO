d2c2133a325effec131c7369e068520fecdf899e
Ivar Vong <ivar@ivarvong.com>
2013-06-17 09:28:43 -0700

    sorting works

--- public/app.js
+++ public/app.js
@@ -3,20 +3,20 @@ function PhotoCtrl($scope) {
 	$scope.photos = [];
 	$scope.socket = io.connect(window.location.protocol + "//" + window.location.host);
 
-	$scope.byCreatedTime = function(photo) {
-  		return parseInt(photo.created_at);
+	$scope.byCreatedTime = function(photo) {		
+  		return -1* parseInt(photo.created_time);
   	}
 
 	$scope.socket.on('bootstrap', function(photos) {
-		console.log('bootstrap', photos);
+		//console.log('bootstrap', photos);
 		$scope.$apply(function(scope) {
 			scope.photos = photos;
 		});
 	});	
 
 	$scope.socket.on('new', function(photo) {
-		console.log('new', photo);
-		$scope.$apply(function(scope){
+		//console.log('new', photo);
+		$scope.$apply(function(scope){			
 			scope.photos.push(photo);
 		});
 	});
